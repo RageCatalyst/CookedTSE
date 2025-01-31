01b22a5bf41c5827c7643706b8461f5ec5bfca16
Luke Davies <lukedavies4073@gmail.com>
2025-01-31 13:35:21 +0000

    Base Work

---
+++ overcooked-tse/Scenes/Scripts/camera_container.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var _camera_offset : Vector3
+@export var player : Node3D
+@export var smooth_speed : float = 5.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player:
+		var _calculated_position := player.global_transform.origin + _camera_offset
+		global_transform.origin = lerp(global_transform.origin, _calculated_position, smooth_speed * delta)
+	
